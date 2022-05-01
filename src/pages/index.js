d86955f071b49154ef487aad328a5ce7bc3dc851
Maksim Nikolaev <mksniklaev@gmail.com>
2022-05-01 13:15:25 +0500

    feat: install webpack and create folder src

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import PopupWithImage from '../components/PopupWithImage.js'
 import PopupWithForm from '../components/PopupWithForm.js'
 import UserInfo from '../components/UserInfo.js'
 import Section from '../components/Section.js'
-import { initialCards } from './data.js'
+import { initialCards } from '../utils/data.js'
 
 export const popupPhoto = document.querySelector('.popup_photo');
 
